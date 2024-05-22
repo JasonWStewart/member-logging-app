3feca5e3d80c4e03818d4144d5b9191a4520a8eb
Jason Stewart <jason@jasonstewart.io>
2024-05-22 16:57:31 +0100

    failed last commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    manifest: true,
-  },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "favicon.svg"],
+      manifest: {
+        name: "HVFC Matchday",
+        short_name: "HVFC MD",
+        description: "HVFC Matchday Dashboard",
+        theme_color: "#C80000",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
